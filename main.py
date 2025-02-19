a86afdbf968f405b6097c1a269d49e5ac41bd21a
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-02-19 20:19:11 -0300

    Começo da interface

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Bem vindo ao SECCOS: Sistema de Estacionamento de Carros Ozzy OSborne\n")
+
+while True:
+    print("Selecione uma opção\n")
+    print("1. Adicionar um carro à fila de entrada.")
+    print("2. Permitir entrada de carro no estacionamento")
+    print("3. Ver fila de entrada")
+    print("4. Ver pilha de saída")
+    print("5. Retirar carro em pilha e gerar preço")
+    escolha = int(input("Escolha: "))
